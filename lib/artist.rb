e436b91de8b817199cbd165ebb86fdbecb7536f2
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-21 17:09:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ class Artist
   attr_reader :songs
 
   extend Memorable::ClassMethods
-  extend Findable
+  # extend Findable
 
   @@artists = []
 
@@ -39,7 +39,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
